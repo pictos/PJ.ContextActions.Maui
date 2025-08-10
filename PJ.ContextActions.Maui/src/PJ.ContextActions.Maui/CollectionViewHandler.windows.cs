753f0dcb3e6fd5baf25f5471bc0e4441af10b075
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-08-09 21:26:31 -0300

    Enhance context actions for Windows collection views
    Introduces `PJCollectionViewHandler` to manage context actions and menu flyouts in collection views on Windows. Removes the obsolete `PlatformClass1.cs`. Adds a static `Helpers` class with a method to retrieve collection view models. Introduces `CommandBag` record for command context. Updates `GlobalUsings.cs` for simplified debugging.

---
+++ PJ.ContextActions.Maui/src/PJ.ContextActions.Maui/CollectionViewHandler.windows.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Maui.Controls.Handlers.Items;
+using Microsoft.UI.Xaml.Controls;
+using WMenuFlyout = Microsoft.UI.Xaml.Controls.MenuFlyout;
+using WMenuFlyoutItem = Microsoft.UI.Xaml.Controls.MenuFlyoutItem;
+
+namespace PJ.ContextActions.Maui;
+
+sealed class PJCollectionViewHandler : CollectionViewHandler
+{
+	MenuItem[]? menuItems;
+	Command<CommandBag>? mauiCommand;
+
+	protected override ListViewBase CreatePlatformView()
+	{
+		var listView = base.CreatePlatformView();
+
+		listView.ContainerContentChanging += OnContainerContentChanging;
+
+		return listView;
+	}
+
+	protected override void DisconnectHandler(ListViewBase platformView)
+	{
+		platformView.ContainerContentChanging -= OnContainerContentChanging;
+		base.DisconnectHandler(platformView);
+	}
+
+	void OnContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+	{
+		if (args.InRecycleQueue || args.ItemContainer is null)
+			return;
+
+		if (menuItems is null)
+		{
+			Assert(VirtualView is not null);
+			var contextActions = ContextActions.GetContextActions((CollectionView)VirtualView);
+
+			menuItems = [.. contextActions];
+		}
+
+		var menuFlyout = new WMenuFlyout();
+		var model = args.Item.ToCollectionViewModel();
+
+		mauiCommand ??= new Command<CommandBag>(static bag =>
+		{
+			bag.item.FireClicked(bag.cvItem);
+			var command = bag.item.Command;
+
+			if (command?.CanExecute(bag.cvItem) is true)
+			{
+				command.Execute(bag.cvItem);
+			}
+		});
+
+		foreach (var item in menuItems)
+		{
+			item.BindingContext = VirtualView.BindingContext;
+
+			menuFlyout.Items.Add(new WMenuFlyoutItem
+			{
+				Text = item.Text,
+				Command = mauiCommand,
+				CommandParameter = new CommandBag(model, item)
+			});
+		}
+
+		args.ItemContainer.ContextFlyout = menuFlyout;
+	}
+}
---
+++ PJ.ContextActions.Maui/src/PJ.ContextActions.Maui/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using static System.Reflection.BindingFlags;
+
+namespace PJ.ContextActions.Maui;
+static class Helpers
+{
+#if WINDOWS
+	static Type? itemTemplateContextType;
+
+	public static object ToCollectionViewModel(this object item)
+	{
+		itemTemplateContextType ??= item.GetType();
+
+		var propertyInfo = itemTemplateContextType.GetProperty("Item", Instance | Public);
+
+		Assert(propertyInfo is not null);
+
+		var value = propertyInfo.GetValue(item, null);
+
+		Assert(value is not null);
+
+		return value;
+	}
+#endif
+}
