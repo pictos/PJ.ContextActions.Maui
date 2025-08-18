cb795c76765f40bc1501be65a79738642457afd4
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-08-17 21:01:25 -0300

    Add context actions support for CollectionView (#4)
    * Add context actions support for CollectionView
    
    Introduce `PJ.ContextActions.Maui` namespace with `AppBuilderExtensions` to configure custom handlers for `CollectionView`. Implement `PJCollectionViewHandler` and `PJViewAdapter` to manage context menus and item interactions. Add `ItemContextMenuListener` and `MenuItemClickListener` for handling context menu creation and item clicks.
    
    * fix type

---
+++ src/PJ.ContextActions.Maui/AppBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace PJ.ContextActions.Maui;
+public static class AppBuilderExtensions
+{
+	public static MauiAppBuilder UseContextActions(this MauiAppBuilder builder)
+	{
+		builder.ConfigureMauiHandlers(h =>
+		{
+			h.AddHandler(typeof(CollectionView), typeof(PJCollectionViewHandler));
+		});
+
+		return builder;
+	}
+}
---
+++ src/PJ.ContextActions.Maui/PJCollectionViewHandler.android.cs
@@ -0,0 +1,142 @@
+﻿using Android.Content;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using Microsoft.Maui.Controls.Handlers.Items;
+
+namespace PJ.ContextActions.Maui;
+
+sealed class PJCollectionViewHandler : CollectionViewHandler
+{
+	protected override ReorderableItemsViewAdapter<ReorderableItemsView, IGroupableItemsViewSource> CreateAdapter()
+	{
+		return new PJViewAdapter(VirtualView);
+	}
+}
+
+
+sealed class PJViewAdapter : ReorderableItemsViewAdapter<ReorderableItemsView, IGroupableItemsViewSource>
+{
+	IGroupableItemsViewSource itemsSource = default!;
+	ReorderableItemsView collectionView;
+
+	internal static MenuItem[]? MenuItems;
+	public PJViewAdapter(ReorderableItemsView reorderableItemsView, Func<Microsoft.Maui.Controls.View, Context, ItemContentView>? createView = null) : base(reorderableItemsView, createView)
+	{
+		collectionView = reorderableItemsView;
+	}
+
+	protected override IGroupableItemsViewSource CreateItemsSource()
+	{
+		return ItemsSource = base.CreateItemsSource();
+	}
+
+	public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+	{
+		base.OnBindViewHolder(holder, position);
+
+		if (collectionView is not CollectionView cv)
+		{
+			return;
+		}
+
+		var contextActions = ContextActions.GetContextActions(cv);
+
+		if (contextActions.Count is 0)
+		{
+			return;
+		}
+
+		if (MenuItems is null)
+		{
+			MenuItems = new MenuItem[contextActions.Count];
+
+			foreach (var (index, item) in contextActions.Index())
+			{
+				item.BindingContext = cv.BindingContext;
+				MenuItems[index] = item;
+			}
+		}
+
+		position -= itemsSource.HasHeader ? 1 : 0;
+
+		var size = itemsSource.Count - (itemsSource.HasHeader ? 1 : 0) - (itemsSource.HasHeader ? 1 : 0);
+
+		if (position >= 0 && position < size)
+		{
+			var element = itemsSource.GetItem(position + 1);
+
+		}
+	}
+
+	public override void OnViewRecycled(Java.Lang.Object holder)
+	{
+		base.OnViewRecycled(holder);
+
+		if (holder is RecyclerView.ViewHolder viewHolder)
+		{
+			viewHolder.ItemView.SetOnCreateContextMenuListener(null);
+		}
+	}
+}
+
+sealed class ItemContextMenuListener : Java.Lang.Object, global::Android.Views.View.IOnCreateContextMenuListener
+{
+	readonly object element;
+
+	public ItemContextMenuListener(object element)
+	{
+		this.element = element;
+	}
+
+	public void OnCreateContextMenu(IContextMenu? menu, Android.Views.View? v, IContextMenuContextMenuInfo? menuInfo)
+	{
+		if (menu is null || v is null)
+		{
+			return;
+		}
+
+		if (PJViewAdapter.MenuItems is null)
+		{
+			return;
+		}
+
+		var menuItems = PJViewAdapter.MenuItems;
+
+		foreach (var (index, item) in menuItems.Index())
+		{
+			var mItem = menu.Add(0, index + 1, index, item.Text);
+			Assert(mItem is not null);
+			mItem.SetOnMenuItemClickListener(new MenuItemClickListener(new(element, item)));
+		}
+	}
+}
+
+sealed class MenuItemClickListener : Java.Lang.Object, IMenuItemOnMenuItemClickListener
+{
+	readonly CommandBag bag;
+
+	public MenuItemClickListener(CommandBag bag)
+	{
+		this.bag = bag;
+	}
+
+	public bool OnMenuItemClick(IMenuItem item)
+	{
+		if (item is null)
+		{
+			return false;
+		}
+
+		var menuItem = bag.item;
+		var element = bag.cvItem;
+
+		menuItem.FireClicked(element);
+
+		if (menuItem.Command?.CanExecute(element) is true)
+		{
+			menuItem.Command.Execute(element);
+		}
+
+		return true;
+	}
+}
