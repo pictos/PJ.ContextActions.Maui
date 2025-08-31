9f37d313ac6ca0a13ff4f556f6e4dd47b105ee7f
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-08-30 22:15:06 -0300

    Enhance context menu functionality across platforms
    Updated project files to support context menus in collection views for Android, iOS, and Windows. Introduced new handler classes and improved project structure by adding a "Controls" folder. Removed platform-specific comments in `PlatformClass1.cs` for a more generic implementation.

---
+++ src/PJ.ContextActions.Maui/MenuItemClickListener.android.cs
@@ -0,0 +1,33 @@
+﻿using Android.Views;
+
+namespace PJ.ContextActions.Maui;
+
+sealed class MenuItemClickListener : Java.Lang.Object, IMenuItemOnMenuItemClickListener
+{
+	readonly CommandBag bag;
+
+	public MenuItemClickListener(CommandBag bag)
+	{
+		this.bag = bag;
+	}
+
+	public bool OnMenuItemClick(IMenuItem item)
+	{
+		if (item is null)
+		{
+			return false;
+		}
+
+		var menuItem = bag.item;
+		var element = bag.cvItem;
+
+		menuItem.FireClicked(element);
+
+		if (menuItem.Command?.CanExecute(element) is true)
+		{
+			menuItem.Command.Execute(element);
+		}
+
+		return true;
+	}
+}
--- src/PJ.ContextActions.Maui/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PJ.ContextActions.Maui;
-
-// All the code in this file is only included on Mac Catalyst.
-public class PlatformClass1
-{
-}
