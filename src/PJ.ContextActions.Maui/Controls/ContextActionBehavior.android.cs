7afe01b05495259bc301eccfc984c9115447c8f5
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-08-30 23:52:08 -0300

    Add Android-specific ContextActionBehavior implementation
    Introduces a new partial class `ContextActionBehavior` for Android, extending `PlatformBehavior<View, AView>`. This implementation adds context menu management capabilities, including a `ContextMenuListenerFactory` for custom listeners. The `OnAttachedTo` and `OnDetachedFrom` methods are overridden to manage the context menu listener lifecycle. A new sealed class `AViewContextMenuListener` is created to handle context menu item setup and click events. The original `ContextActionBehavior` class is updated to remove Android-specific code.

---
+++ src/PJ.ContextActions.Maui/Controls/ContextActionBehavior.android.cs
@@ -0,0 +1,48 @@
+﻿using Android.Views;
+using AView = Android.Views.View;
+using View = Microsoft.Maui.Controls.View;
+
+namespace PJ.ContextActions.Maui;
+partial class ContextActionBehavior : PlatformBehavior<View, AView>
+{
+	public Func<AView.IOnCreateContextMenuListener>? ContextMenuListenerFactory { get; set; }
+
+	protected override void OnAttachedTo(View bindable, AView platformView)
+	{
+		var contextMenuListener = ContextMenuListenerFactory?.Invoke() ?? new AViewContextMenuListener([.. MenuItems], bindable);
+		platformView.SetOnCreateContextMenuListener(contextMenuListener);
+	}
+
+	protected override void OnDetachedFrom(View bindable, AView platformView)
+	{
+		platformView.SetOnCreateContextMenuListener(null);
+	}
+}
+
+sealed class AViewContextMenuListener : Java.Lang.Object, AView.IOnCreateContextMenuListener
+{
+	readonly MenuItem[] menuItems;
+	readonly View view;
+
+	public AViewContextMenuListener(MenuItem[] menuItems, View view)
+	{
+		this.menuItems = menuItems;
+		this.view = view;
+	}
+
+	public void OnCreateContextMenu(Android.Views.IContextMenu? menu, AView? v, Android.Views.IContextMenuContextMenuInfo? menuInfo)
+	{
+		if (menu is null || v is null)
+		{
+			return;
+		}
+
+		foreach (var (index, item) in menuItems.Index())
+		{
+			item.BindingContext = view.BindingContext;
+			var mItem = menu.Add(0, index + 1, index, item.Text);
+			Assert(mItem is not null);
+			mItem.SetOnMenuItemClickListener(new MenuItemClickListener(new(view, item)));
+		}
+	}
+}
--- src/PJ.ContextActions.Maui/Controls/ContextActionBehavior.cs
+++ src/PJ.ContextActions.Maui/Controls/ContextActionBehavior.cs
@@ -3,13 +3,4 @@
 public partial class ContextActionBehavior
 {
 	public ICollection<MenuItem> MenuItems { get; set; } = [];
-
 }
-
-
-#if ANDROID
-partial class ContextActionBehavior : PlatformBehavior<View, Android.Views.View>
-{
-
-}
-#endif
